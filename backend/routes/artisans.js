3e8709ecebf58ea32d5a8433b7efe3331a3b19de
codewithswatiee <verma.s@somaiya.edu>
2024-10-17 17:41:33 +0530

    starting from scratch

--- backend/routes/artisans.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {upload, createProduct} = require("../controllers/productControllers");
-
-router.post('/products', upload.single('image'), createProduct);
-
-module.exports = router;
